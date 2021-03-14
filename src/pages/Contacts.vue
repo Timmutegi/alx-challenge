f4f4253e77b6161e0ee1183475c129fb903c33e0
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-14 18:37:20 +0200

    Added form

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding >
     <div class="button">
-      <q-btn color="primary" no-caps label="Add contact" class="btn my-font"/>
+      <q-btn color="primary" no-caps label="Add contact" @click="prompt = true" class="btn my-font"/>
     </div>
 
     <q-table
@@ -13,6 +13,48 @@
       :selected.sync="selected"
       :pagination="initialPagination"
     />
+
+    <q-dialog v-model="prompt">
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <!-- <q-form
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-md"
+          >
+            <q-input
+              v-model="name"
+              placeholder='Jane'
+              label="Name"
+              lazy-rules
+              :rules="[ val => val && val.length > 0 || 'Please input the name']"
+            />
+
+            <q-select v-model="model" :options="options" />
+
+            <q-input
+              v-model="associated"
+              placeholder='Lindsey Stroud '
+              lazy-rules
+              :rules="[ val => val && val.length > 0 || 'Please type something']"
+            />
+
+            <q-input
+              v-model="title"
+              placeholder='CEO'
+              label="Title"
+              lazy-rules
+              :rules="[ val => val && val.length > 0 || 'Please input the title']"
+            />
+
+            <div>
+              <q-btn label="Submit" type="submit" color="primary"/>
+              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+          </q-form> -->
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -20,26 +62,30 @@
 export default {
   data () {
     return {
+      options: [
+        'Office meet-up', 'Call', 'Event'
+      ],
+      prompt: false,
       initialPagination: {
         page: 1,
         rowsPerPage: 10
       },
       selected: [],
       columns: [
-        { name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true },
-        { name: 'email', align: 'left', label: 'Email', field: 'email' },
-        { name: 'company_name', align: 'left', label: 'Company name', field: 'company_name' },
-        { name: 'role', align: 'left', label: 'Role', field: 'role' },
-        { name: 'forecast', align: 'left', label: 'Forecast', field: 'forecast' },
-        { name: 'recent_activity', align: 'left', label: 'Recent Activity', field: 'recent_activity' }
+        { name: 'name', align: 'left', label: 'Name', field: 'name', style: 'font-weight: 600; font-size: 15px;' },
+        { name: 'email', align: 'left', label: 'Email', field: 'email', classes: 'data' },
+        { name: 'company_name', align: 'left', label: 'Company name', field: 'company_name', classes: 'data' },
+        { name: 'role', align: 'left', label: 'Role', field: 'role', classes: 'data' },
+        { name: 'forecast', align: 'left', label: 'Forecast', field: 'forecast', classes: 'data' },
+        { name: 'recent_activity', align: 'left', label: 'Recent Activity', field: 'recent_activity', classes: 'data' }
 
       ],
       data: [
         {
           name: 'Lindsey Stroud',
-          email: 'linsey.stroud@gmail.com',
+          email: 'lindsey.stroud@gmail.com',
           company_name: 'Hatchbuck',
-          role: 'manager',
+          role: 'Manager',
           forecast: '50 %',
           recent_activity: '5 Minutes ago'
         },
@@ -47,7 +93,7 @@ export default {
           name: 'Nicci Troiani',
           email: 'nicci.troiani@gmail.com',
           company_name: 'Slack',
-          role: 'manager',
+          role: 'Manager',
           forecast: '75 %',
           recent_activity: '14 Minutes ago'
         },
@@ -144,8 +190,17 @@ export default {
   }
   .table {
     margin-top: 20px;
+    th {
+      color: #707683;
+      font-weight: 600;
+    }
     tr {
       height: 64px;
+      font-weight: 600;
+    }
+    .data {
+      color: #707683;
+      font-weight: 600;
     }
   }
 </style>
