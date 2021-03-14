46010feaab343e2ba86887c29efeede070799964
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-14 18:37:45 +0200

    removed comments

--- src/pages/LineChart.js
+++ src/pages/LineChart.js
@@ -6,14 +6,6 @@ export default {
   mixins: [reactiveProp],
   props: ['options'],
   mounted () {
-    // this.chartData is created in the mixin.
-    // If you want to pass options please create a local options object
-    // var ctx = document.getElementById('line-chart').getContext('2d')
-
-    // var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0)
-    // gradientStroke.addColorStop(0, '#109cf1')
-    // gradientStroke.addColorStop(1, '#f49080')
-
     this.renderChart(this.chartData, this.options)
   }
 }
