969dc4617555c9136f1e28ce24ca286c8df29061
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-19 13:16:51 +0200

    Fixed deployment tot heroku

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const
 
 const app = express()
 
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }))
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
-
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/spa/index.html'));
-// })
-
 app.listen(port)
