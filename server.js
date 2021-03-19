20bd89fbc8a6772e856c3a9f9191bb7930076f6b
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-19 13:01:57 +0200

    updated configurations

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const
 
 const app = express()
 
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
-
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }))
+app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/spa/index.html'));
-})
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/spa/index.html'));
+// })
 
 app.listen(port)
