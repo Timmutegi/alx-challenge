a514f3b9541a2fc7ea123eac9fb84a94f9013f77
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-21 19:35:08 +0200

    Created doughnut chart

---
+++ src/pages/DoughNut.js
@@ -0,0 +1,21 @@
+import { Doughnut, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Doughnut,
+  data: () => ({
+    options: {
+      legend: {
+        display: true,
+        position: 'right'
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      cutoutPercentage: 90
+    }
+  }),
+  mixins: [reactiveProp],
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
