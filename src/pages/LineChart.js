fb43780f516cf871c140b007a98cf53acbc785a2
Timmutegi <mutegi.timothy538@gmail.com>
2021-03-21 19:35:32 +0200

    refactored linechart

--- src/pages/LineChart.js
+++ src/pages/LineChart.js
@@ -3,8 +3,43 @@ const { reactiveProp } = mixins
 
 export default {
   extends: Line,
+  data: () => ({
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        alignment: 'start',
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          type: 'linear',
+          display: true,
+          gridLines: {
+            borderDash: [10, 6],
+            zeroLineColor: 'transparent'
+          },
+          scaleLabel: {
+            display: true,
+            fontStyle: 'bold'
+          },
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 10,
+            stepSize: 50,
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          gridLines: {
+            color: '#FFFFFF',
+            zeroLineColor: 'transparent'
+          }
+        }]
+      }
+    }
+  }),
   mixins: [reactiveProp],
-  props: ['options'],
   mounted () {
     this.renderChart(this.chartData, this.options)
   }
